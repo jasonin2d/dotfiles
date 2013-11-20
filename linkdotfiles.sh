c2a56a4b9bebce0d52f24f279aa67d3da0859df0
Jason <jasonin2d@gmail.com>
2013-11-20 15:43:47 +0000

    Added linkdotfiles.sh, to automate backing original dotfiles and creating of symlinks

---
+++ linkdotfiles.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Script to create symlinks back to contents of dotfiles directory
+
+files="tmux.conf vimrc"
+
+dotdir=~/dotfiles
+olddotdir=~/olddotfiles
+
+mkdir -p $olddotdir
+
+for file in $files; do
+    if [ -f ~/.$file ]
+        then
+            echo "Backing up old copy of .$file"
+            mv ~/.$file $olddotdir
+    fi
+    echo "Creating symlink for ~/dotfiles/$file"
+    ln -s $dotdir/$file ~/.$file 
+    echo "...done"
+done
+
